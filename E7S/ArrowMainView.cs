d77b5f08452318eaac76d7b60ed1bf8e8e4634ca
Esjihn <sysnom@gmail.com>
2020-10-25 21:53:43 -0400

    Add project files.

---
+++ E7S/ArrowMainView.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace E7S
+{
+    public partial class ArrowMainView : Form
+    {
+        public ArrowMainView()
+        {
+            InitializeComponent();
+            LoadArrowUserControl();
+        }
+
+        private void LoadArrowUserControl()
+        {
+            ArrowUserControl auc = new ArrowUserControl();
+            this.Controls.Add(auc);
+            auc.Dock = DockStyle.Fill;
+        }
+    }
+}
